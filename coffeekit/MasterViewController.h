4a0d8c471759abbe027b72581d7606713ace56d4
Giau Ngo <giaunv@gmail.com>
2015-04-03 06:57:17 +0700

    Project Setup

---
+++ coffeekit/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  coffeekit
+//
+//  Created by giaunv on 4/3/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
